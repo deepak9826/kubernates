04183a73f2668d970ba59ec66602b82845d0354d
Deepak Malviya <deepak.malviya@hoonartek.com>
2023-12-05 15:09:38 +0530

    initial commit

---
+++ src/main/java/com/example/kubernates/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.kubernates.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	
+	@GetMapping("/message")
+	public String display(){
+		return "Congratulation you successfully...";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
